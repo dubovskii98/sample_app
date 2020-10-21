d3835617249e50a891e052f4321036a05073133c
dubovskii98 <dubovskii98@gmail.com>
2020-10-21 13:43:40 +0300

    fix issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors =true
   config.action_mailer.delivery_method =:smtp
   host ='sampleapp-dubovskyi.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'domain.com',
-      :address => 'smtp.sendgrid.net',
-      :port => 2525,
-      :authentication => :plain,
-      :enable_starttls_auto => true
+      :address=>'smtp.sendgrid.net',
+      :port=>'587',
+      :authentication=>:plain,
+      :user_name=>ENV['dubovskii98@gmail.com'],
+      :password=>ENV['vamosarribalaceleste'],
+      :domain=>'heroku.com',
+      :enable_starttls_auto=>true
   }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
